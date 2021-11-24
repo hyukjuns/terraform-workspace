5d21e17f9c855070adc172f75a4ab2c4247c4d34
namhj94 <hyukjun1994@gmail.com>
2021-11-24 14:28:14 +0900

    update network

--- network-workspace/main.tf
+++ network-workspace/main.tf
@@ -24,13 +24,17 @@ resource "azurerm_resource_group" "network" {
   location = var.location
 }
 
-resource "azurerm_virtual_network" "network_dev" {
-  name                = "${var.prefix}-vnet-${var.suffix}"
-  resource_group_name = azurerm_resource_group.network.name
-  location            = azurerm_resource_group.network.location
-  address_space       = ["10.0.0.0/16"]
-  subnet {
-    name           = "dev-01"
-    address_prefix = "10.0.0.0/24"
-  }
+module "network" {
+  source  = "app.terraform.io/cloocus-mspdevops/network/azurerm"
+  version = "1.0.0"
+  resource_group_name   = azurerm_resource_group.rg.name
+  location              = azurerm_resource_group.rg.location
+  vnet_name             = "${var.prefix}-vnet-${var.suffix}"
+  address_space         = ["10.0.0.0/16"]
+  subnet_name           = "dev-01"
+  subnet_address_prefixes = ["10.0.0.0/24"]
+
+  depends_on = [
+    azurerm_resource_group.rg
+  ]
 }
--- network-workspace/outputs.tf
+++ network-workspace/outputs.tf
@@ -1,3 +1,3 @@
 output "subnet_dev_01_id" {
-  value = azurerm_virtual_network.network_dev.subnet.*.id[0]
+  value = module.network.subnet_id
 }
