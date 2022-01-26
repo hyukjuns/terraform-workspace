58e1e2e99205b7cdd55028c56b1ccef8e67a11a9
namhj94 <hyukjun1994@gmail.com>
2022-01-26 13:26:33 +0900

    update network, vm

--- tfcloud-network-workspace/main.tf
+++ tfcloud-network-workspace/main.tf
@@ -39,3 +39,41 @@ module "network" {
     azurerm_resource_group.network
   ]
 }
+# NSG
+module "nsg" {
+  source              = "app.terraform.io/cloocus-mspdevops/nsg/azurerm"
+  version             = "1.0.1"
+  resource_group_name = azurerm_resource_group.module.name
+  location            = azurerm_resource_group.module.location
+  nsg_name            = "${var.prefix}-nsg"
+
+  rules = [
+    {
+      name                       = "ssh"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+      destination_address_prefix = "*"
+      destination_port_range     = "22"
+    },
+    {
+      name                       = "http"
+      priority                   = 110
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_address_prefix      = "*"
+      source_port_range          = "*"
+      destination_address_prefix = "*"
+      destination_port_range     = "80,8080"
+    }
+  ]
+
+  attach_to_subnet = [module.network.subnet_id]
+  depends_on = [
+    azurerm_resource_group.network
+  ]
+}
